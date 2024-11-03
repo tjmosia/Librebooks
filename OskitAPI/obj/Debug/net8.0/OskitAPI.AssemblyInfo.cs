102261ee79182dab040cd7933956e323b94ea306
Mosia Thabo <57020459+tjmosia@users.noreply.github.com>
2024-11-03 21:33:43 +0200

    V2411032133

--- OskitAPI/obj/Debug/net8.0/OskitAPI.AssemblyInfo.cs
+++ OskitAPI/obj/Debug/net8.0/OskitAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OskitAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb4f9d2508d83952d0bb6a9964666487e31bc1f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db53ef172b7859e0b8cafb62616e4b8d92e3ad56")]
 [assembly: System.Reflection.AssemblyProductAttribute("OskitAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OskitAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
